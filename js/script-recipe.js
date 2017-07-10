d18e4bbf58eb82b4a74c822c72b7c913f7e6ec11
ValLR <valentinalopezrecabal@gmail.com>
2017-07-10 16:34:38 -0400

    Etapa 9: Utilizando los métodos addClass y removeClass, dentro de las mismas funciones onclick de los botones en la etapa 8, remuevo la clase aactive del botón no clickeado y la agrego al botón clickeado

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
 	console.log("it's ready, bae")
 	$(".js-show-recipe").click(function(){
 		$("#bod").removeClass("make");
+		$(".js-show-recipe").addClass("active");
+		$(".js-show-make").removeClass("active");
 	})
 	$(".js-show-make").click(function(){
 		$("#bod").addClass("make");
-	})
+		$(".js-show-make").addClass("active");
+		$(".js-show-remove").removeClass("active");
+			})
 
 });
