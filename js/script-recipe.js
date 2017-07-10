7a9eb69b0a328a110d800c592e773da2481c1b7c
ValLR <valentinalopezrecabal@gmail.com>
2017-07-10 16:28:59 -0400

    Etapa 8: usando removeClass y addClass, agrego un evento on click en cada botón, para quitar y agregar la clase 'make'

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	console.log("it's ready, bae")
+	$(".js-show-recipe").click(function(){
+		$("#bod").removeClass("make");
+	})
+	$(".js-show-make").click(function(){
+		$("#bod").addClass("make");
+	})
+
 });
