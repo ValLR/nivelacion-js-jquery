5dc91246d64b4525bec5d67b93c62d84ce32181c
ValLR <valentinalopezrecabal@gmail.com>
2017-07-10 11:37:46 -0400

    Etapa 3: utilizamos un for para recorrer cada elemento del arreglo de recetas

--- js/script-index.js
+++ js/script-index.js
@@ -14,6 +14,9 @@ $(document).ready( function(){
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
+	for(var i=0; i < recipesArray.length; i++){
+		if(recipesArray[i])
+	}
 	console.log('Recipes: ', recipesArray);
 }
 
